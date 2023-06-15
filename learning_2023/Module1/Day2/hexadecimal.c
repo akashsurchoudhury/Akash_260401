dc94a0ef8d12b3179bfc974d80411a48f01888d6
akashsurchoudhury <131943003+akashsurchoudhury@users.noreply.github.com>
2023-06-15 11:43:03 +0530

    Create hexadecimal.c

---
+++ learning_2023/Module1/Day2/hexadecimal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void printExponent(double num) {
+    unsigned long long* ptr = (unsigned long long*)&num;
+    unsigned long long bits = *ptr;
+    unsigned int exponent = (bits >> 52) & 0x7FF;
+
+    printf("Exponent in hexadecimal: 0x%X\n", exponent);
+    printf("Exponent in binary: 0b");
+    for (int i = 10; i >= 0; i--) {
+        printf("%d", (exponent >> i) & 1);
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(x);
+
+    return 0;
+}
