dc09c274d9ab66edd567a429a5cc2de7a7448c35
akashsurchoudhury <131943003+akashsurchoudhury@users.noreply.github.com>
2023-06-24 20:57:34 +0530

    Create allm.c

---
+++ learning_2023/Module1/Day 6/allm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+void initialize(struct Student* students, int size) {
+    for (int i = 0; i < size; i++) {
+        students[i].rollno = 0;
+        strcpy(students[i].name, "");
+        students[i].marks = 0.0;
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of students: ");
+    scanf("%d", &size);
+
+    struct Student* students = (struct Student*)malloc(size * sizeof(struct Student));
+
+    if (students == NULL) {
+        printf("Memory allocation failed.\n");
+        return 0;
+    }
+
+    initialize(students, size);
+
+    printf("Initialized student details:\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("Roll No: %d, Name: %s, Marks: %.2f\n", students[i].rollno, students[i].name, students[i].marks);
+    }
+
+    free(students);
+
+    return 0;
+}
